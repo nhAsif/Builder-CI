c7118544ce2c6bf52d19fe08245238ebd6a9c923
NFS86 <jarbull86@gmail.com>
2022-02-17 18:39:41 +0700

    Fix sync error
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone --depth=1 https://github.com/NFS-projects/local_manifest -b PE-12 .repo/local_manifests
-repo sync -c -v --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
