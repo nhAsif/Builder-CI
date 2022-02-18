7e187e57c5c85a691d9526d6c4c2db7d91831fe9
NFS86 <jarbull86@gmail.com>
2022-02-18 09:16:22 +0700

    Test download ccache witch aria2c
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- load.sh
+++ load.sh
@@ -5,7 +5,8 @@ cd $CIRRUS_WORKING_DIR
 mkdir -p ~/.config/rclone
 echo "$rcloneconfig" > ~/.config/rclone/rclone.conf
 mkdir -p $CIRRUS_WORKING_DIR/ccache
-rclone copy NFS:ccache/$name_rom/ccache.tar.gz /home/cirrus-ci-build -P
+url=https://needforspeed.projek.workers.dev/ccache/$name_rom/ccache.tar.gz
+time aria2c $url -x16 -s50
 time tar xf ccache.tar.gz
 rm -rf ccache.tar.gz
 cat > /etc/ccache.conf <<EOF
