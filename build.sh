03a99ebc57bf49ec21377b62450d33d189d03719
NFS86 <jarbull86@gmail.com>
2022-02-20 10:00:08 +0700

    Test Octavios 12
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone --depth=1 https://github.com/NFS-projects/local_manifest -b PE-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-projects/local_manifest --depth 1 -b oct-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
+lunch octavi_rosy-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
-make bacon &
+brunch rosy &
 sleep 90m
 kill %1
 
