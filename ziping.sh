01297b304f03bf58efda65c03c92eb55b328e7bb
NFS86 <jarbull86@gmail.com>
2022-02-24 05:04:41 +0000

    Final
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- ziping.sh
+++ ziping.sh
@@ -6,7 +6,7 @@ device=$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d 
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 
 function upload_rom() {
-   JOS=WORKDIR/rom/$name_rom/out/target/product/$device/$name_rom*.zip
+   JOS=$WORKDIR/rom/$name_rom/out/target/product/$device/$name_rom*.zip
    curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d disable_web_page_preview=true -d parse_mode=html -d text="<b>Build status:</b>%0A@Bella_Aprilia_27 <code>Building Rom $name_rom succes [✔️]</code>"
    rclone copy --drive-chunk-size 256M --stats 1s $JOS NFS:rom/$name_rom -P
    curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d disable_web_page_preview=true -d parse_mode=html -d text="Link : https://needforspeed.projek.workers.dev/rom/$name_rom/$(cd $WORKDIR/rom/$name_rom/out/target/product/$device && ls $name_rom*.zip)"
