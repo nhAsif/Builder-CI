2b2cdfe3e7e3913216fe5c3982f7d18df9aafc09
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-27 22:35:34 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b Pixel-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b arrow-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
-export TZ=Asia/Jakarta
+lunch arrow_rosy-userdebug
+export TZ=Asia/Dhaka
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka bacon -j10 &
-sleep 100m
+m bacon -j10 &
+sleep 15m
 kill %1
 
 # upload rom
