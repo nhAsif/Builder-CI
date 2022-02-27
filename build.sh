c394623c2b858a979da3940bcf106646715ed96a
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-27 19:14:55 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b PE-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
-export TZ=Asia/Jakarta
+lunch awaken_rosy-userdebug
+export TZ=Asia/Dhaka
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
