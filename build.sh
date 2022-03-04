64430c7cb4e7da2c0d281a76528bb319001385a7
Asif <najmulhasan3609@gmail.com>
2022-03-04 03:00:38 +0000

    rosy: init build [skip ci]

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b arrow-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_rosy-userdebug
+lunch lineage_rosy-userdebug
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-m bacon
+make api-stubs-docs || echo no problem
+make hiddenapi-lists-docs || echo no problem
+make system-api-stubs-docs || echo no problem
+make test-api-stubs-docs || echo no problem
+make bacon -j10 &
+sleep 90m
+kill %1
 
 
 # upload rom
