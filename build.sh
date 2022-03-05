94641f86a2608d85c50f5c0b22658ed5329f2c00
Asif <49162687+nhAsif@users.noreply.github.com>
2022-03-05 12:11:28 +0600

    Final

--- build.sh
+++ build.sh
@@ -10,9 +10,7 @@ export TZ=Asia/Dhaka
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-make bacon -j10 &
-sleep 95m
-kill %1 
+make bacon -j10
 
 
 # upload rom
