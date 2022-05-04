01161989345577423236d317af380363d5da3417
Asif <49162687+nhAsif@users.noreply.github.com>
2022-05-04 13:43:50 +0600

    test build alioth

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
 # build rom
 source build/envsetup.sh
-lunch lineage_rosy-userdebug
+lunch lineage_alioth-userdebug
 export TZ=Asia/Dhaka
-export BUILD_USERNAME=rosy
+export BUILD_USERNAME=alioth
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 make bacon -j10
-
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
