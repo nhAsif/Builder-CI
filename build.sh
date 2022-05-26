60e12e42816a9b407c44b2122ee96d9ca945d6ce
Asif <49162687+nhAsif@users.noreply.github.com>
2022-05-26 09:48:08 +0600

    ci: test build-15

--- build.sh
+++ build.sh
@@ -10,7 +10,10 @@ export TZ=Asia/Dhaka
 export BUILD_USERNAME=alioth
 export BUILD_HOSTNAME=userdebug
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka bacon
+mka bacon &
+sleep 90m
+kill %1 
+
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
