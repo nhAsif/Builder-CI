6977b4d9299ccc28d04056b53998786ac52e0eb1
Asif <49162687+nhAsif@users.noreply.github.com>
2022-05-26 09:20:22 +0600

    ci: test build-14

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j69
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
