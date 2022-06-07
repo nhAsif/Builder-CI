af57505c0bd705f101166d5b19c632c89c4a353e
Asif <49162687+nhAsif@users.noreply.github.com>
2022-06-07 14:56:25 +0600

    ci: blaze-test: 10

--- build.sh
+++ build.sh
@@ -13,6 +13,5 @@ brunch alioth &
 sleep 90m
 kill %1 
 
-
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
