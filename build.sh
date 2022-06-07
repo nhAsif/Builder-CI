166c3de344aa4f1a3bc06fe9b32813d2d268b44a
Asif <najmulhasan3609@gmail.com>
2022-06-07 01:44:17 +0000

    ci: blaze-test: 1

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b crd12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
 # build rom
 source build/envsetup.sh
-lunch lineage_alioth-userdebug
+export WITH_GAPPS=true
+lunch blaze_alioth-userdebug
 export TZ=Asia/Dhaka
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-m bacon &
+brunch alioth &
 sleep 90m
 kill %1 
 
