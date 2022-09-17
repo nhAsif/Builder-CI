ecabf2dc5159706e9d801650f6c66cb21139a001
Asif <49162687+nhAsif@users.noreply.github.com>
2022-09-17 17:17:26 +0600

    alioth: test evo-1

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b evo13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_alioth-userdebug
+lunch evolution_alioth-userdebug
 export TZ=Asia/Dhaka
 export BUILD_USERNAME=alioth
 export BUILD_HOSTNAME=userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka bacon &
+mka evolution &
 sleep 90m
 kill %1 
-
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
