fd8ff9cbdc9fc135e63ff5f7ba729ca21ee14eec
Asif <najmulhasan3609@gmail.com>
2022-10-01 11:17:06 +0000

    ci: test build-1

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
-#sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b evo13 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b xd13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_alioth-userdebug
+lunch xdroid_alioth-userdebug
+export BUILD_USERNAME=nhAsif
+export BUILD_HOSTNAME=pricexd
+export KBUILD_BUILD_USER=nhAsif
+export KBUILD_BUILD_HOST=pricexd
 export TZ=Asia/Dhaka
-export BUILD_USERNAME=alioth
-export BUILD_HOSTNAME=userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka evolution
+make xd
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
