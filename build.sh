6ff15d5645010217f5d7fcee849e074e78e9de6f
Asif <49162687+nhAsif@users.noreply.github.com>
2022-11-10 20:50:35 +0600

    ci: pe test-1

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b evo13 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b pe13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_alioth-userdebug
+lunch aosp_alioth-user
+export KBUILD_BUILD_USER=nhAsif
+export KBUILD_BUILD_HOST=pricexd
+export BUILD_USERNAME=nhAsif
+export BUILD_HOSTNAME=pricexd
 export TZ=Asia/Dhaka
-export BUILD_USERNAME=alioth
-export BUILD_HOSTNAME=userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka evolution
+mka bacon
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
