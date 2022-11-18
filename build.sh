14ea1ab450d71844e83df7965b8f3fa502ac3d6b
Asif <49162687+nhAsif@users.noreply.github.com>
2022-11-18 20:32:26 +0600

    alioth: test evo-6

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
 # build rom
 source build/envsetup.sh
-lunch evolution_alioth-userdebug
+lunch evolution_alioth-user
 export TZ=Asia/Dhaka
-export BUILD_USERNAME=alioth
-export BUILD_HOSTNAME=userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-mka evolution
+mka evolution &
+sleep 90m
+kill %1
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
